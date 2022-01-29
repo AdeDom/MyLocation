0e1fbbf235d9bc375eddeb97ea87b3be0576eac5
pathiphon <pathiphon.ja@chococrm.com>
2022-01-29 13:24:43 +0700

    Clean comment and log

--- app/src/main/java/com/adedom/mylocation/ForegroundServiceActivity.kt
+++ app/src/main/java/com/adedom/mylocation/ForegroundServiceActivity.kt
@@ -1,18 +1,3 @@
-/*
- * Copyright 2019 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.adedom.mylocation
 
 import android.Manifest
@@ -23,65 +8,20 @@ import android.net.Uri
 import android.os.Bundle
 import android.os.IBinder
 import android.provider.Settings
-import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import androidx.core.app.ActivityCompat
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.google.android.material.snackbar.Snackbar
 
-private const val TAG = "ForegroundServiceActivity"
 private const val REQUEST_FOREGROUND_ONLY_PERMISSIONS_REQUEST_CODE = 34
 
-/**
- *  This app allows a user to receive location updates without the background permission even when
- *  the app isn't in focus. This is the preferred approach for Android.
- *
- *  It does this by creating a foreground service (tied to a Notification) when the
- *  user navigates away from the app. Because of this, it only needs foreground or "while in use"
- *  location permissions. That is, there is no need to ask for location in the background (which
- *  requires additional permissions in the manifest).
- *
- *  Note: Users have the following options in Android 11+ regarding location:
- *
- *  * Allow all the time
- *  * Allow while app is in use, i.e., while app is in foreground (new in Android 10)
- *  * Allow one time use (new in Android 11)
- *  * Not allow location at all
- *
- * It is generally recommended you only request "while in use" location permissions (location only
- * needed in the foreground), e.g., fine and coarse. If your app has an approved use case for
- * using location in the background, request that permission in context and separately from
- * fine/coarse location requests. In addition, if the user denies the request or only allows
- * "while-in-use", handle it gracefully. To see an example of background location, please review
- * {@link https://github.com/android/location-samples/tree/master/LocationUpdatesBackgroundKotlin}.
- *
- * Android 10 and higher also now requires developers to specify foreground service type in the
- * manifest (in this case, "location").
- *
- * For the feature that requires location in the foreground, this sample uses a long-running bound
- * and started service for location updates. The service is aware of foreground status of this
- * activity, which is the only bound client in this sample.
- *
- * While getting location in the foreground, if the activity ceases to be in the foreground (user
- * navigates away from the app), the service promotes itself to a foreground service and continues
- * receiving location updates.
- *
- * When the activity comes back to the foreground, the foreground service stops, and the
- * notification associated with that foreground service is removed.
- *
- * While the foreground service notification is displayed, the user has the option to launch the
- * activity from the notification. The user can also remove location updates directly from the
- * notification. This dismisses the notification and stops the service.
- */
 class ForegroundServiceActivity : BaseLocationActivity(),
     SharedPreferences.OnSharedPreferenceChangeListener {
     private var foregroundOnlyLocationServiceBound = false
 
-    // Provides location updates for while-in-use feature.
     private var foregroundOnlyLocationService: ForegroundOnlyLocationService? = null
 
-    // Listens for location broadcasts from ForegroundOnlyLocationService.
     private lateinit var foregroundOnlyBroadcastReceiver: ForegroundOnlyBroadcastReceiver
 
     private lateinit var sharedPreferences: SharedPreferences
@@ -90,7 +30,6 @@ class ForegroundServiceActivity : BaseLocationActivity(),
 
     private lateinit var outputTextView: TextView
 
-    // Monitors connection to the while-in-use service.
     private val foregroundOnlyServiceConnection = object : ServiceConnection {
 
         override fun onServiceConnected(name: ComponentName, service: IBinder) {
@@ -126,10 +65,8 @@ class ForegroundServiceActivity : BaseLocationActivity(),
             if (enabled) {
                 foregroundOnlyLocationService?.unsubscribeToLocationUpdates()
             } else {
-                // TODO: Step 1.0, Review Permissions: Checks and requests if needed.
                 if (foregroundPermissionApproved()) {
                     foregroundOnlyLocationService?.subscribeToLocationUpdates()
-                        ?: Log.d(TAG, "Service Not Bound")
                 } else {
                     requestForegroundPermissions()
                 }
@@ -177,7 +114,6 @@ class ForegroundServiceActivity : BaseLocationActivity(),
     }
 
     override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences, key: String) {
-        // Updates button states if new while in use location is added to SharedPreferences.
         if (key == SharedPreferenceUtil.KEY_FOREGROUND_ENABLED) {
             updateButtonState(
                 sharedPreferences.getBoolean(
@@ -187,7 +123,6 @@ class ForegroundServiceActivity : BaseLocationActivity(),
         }
     }
 
-    // TODO: Step 1.0, Review Permissions: Method checks if permissions approved.
     private fun foregroundPermissionApproved(): Boolean {
         return PackageManager.PERMISSION_GRANTED == ActivityCompat.checkSelfPermission(
             this,
@@ -195,12 +130,9 @@ class ForegroundServiceActivity : BaseLocationActivity(),
         )
     }
 
-    // TODO: Step 1.0, Review Permissions: Method requests permissions.
     private fun requestForegroundPermissions() {
         val provideRationale = foregroundPermissionApproved()
 
-        // If the user denied a previous request, but didn't check "Don't ask again", provide
-        // additional rationale.
         if (provideRationale) {
             Snackbar.make(
                 findViewById(R.id.activity_main),
@@ -208,7 +140,6 @@ class ForegroundServiceActivity : BaseLocationActivity(),
                 Snackbar.LENGTH_LONG
             )
                 .setAction(R.string.ok) {
-                    // Request permission
                     ActivityCompat.requestPermissions(
                         this@ForegroundServiceActivity,
                         arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
@@ -217,7 +148,6 @@ class ForegroundServiceActivity : BaseLocationActivity(),
                 }
                 .show()
         } else {
-            Log.d(TAG, "Request foreground only permission")
             ActivityCompat.requestPermissions(
                 this@ForegroundServiceActivity,
                 arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
@@ -232,19 +162,11 @@ class ForegroundServiceActivity : BaseLocationActivity(),
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        Log.d(TAG, "onRequestPermissionResult")
 
         when (requestCode) {
-            REQUEST_FOREGROUND_ONLY_PERMISSIONS_REQUEST_CODE -> when {
-                grantResults.isEmpty() ->
-                    // If user interaction was interrupted, the permission request
-                    // is cancelled and you receive empty arrays.
-                    Log.d(TAG, "User interaction was cancelled.")
-                grantResults[0] == PackageManager.PERMISSION_GRANTED ->
-                    // Permission was granted.
-                    foregroundOnlyLocationService?.subscribeToLocationUpdates()
+            REQUEST_FOREGROUND_ONLY_PERMISSIONS_REQUEST_CODE -> when (PackageManager.PERMISSION_GRANTED) {
+                grantResults[0] -> foregroundOnlyLocationService?.subscribeToLocationUpdates()
                 else -> {
-                    // Permission denied.
                     updateButtonState(false)
 
                     Snackbar.make(
@@ -253,7 +175,6 @@ class ForegroundServiceActivity : BaseLocationActivity(),
                         Snackbar.LENGTH_LONG
                     )
                         .setAction(R.string.settings) {
-                            // Build intent that displays the App settings screen.
                             val intent = Intent()
                             intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
                             val uri = Uri.fromParts(
@@ -286,9 +207,6 @@ class ForegroundServiceActivity : BaseLocationActivity(),
         outputTextView.text = outputWithPreviousLogs
     }
 
-    /**
-     * Receiver for location broadcasts from [ForegroundOnlyLocationService].
-     */
     private inner class ForegroundOnlyBroadcastReceiver : BroadcastReceiver() {
 
         override fun onReceive(context: Context, intent: Intent) {
